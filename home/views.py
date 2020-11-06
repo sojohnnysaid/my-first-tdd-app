68a18bedde8ec7804c7cbabafb01c0347132c8d2
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-05 22:10:59 -0500

    updated homepage

--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    submitted_word = request.POST.get('item_to_reverse', '')
-    reversed_word = submitted_word[::-1]
-    return render(request, 'home/home.html',{'reversed_word': reversed_word})
+    return render(request, 'home/home.html')
