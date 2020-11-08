2438d97ecfbdc712abd7a8981cdbb386ead8f4b2
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-07 22:11:29 -0500

    removed dice app

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'reverse.apps.ReverseConfig',
-    'dice.apps.DiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
