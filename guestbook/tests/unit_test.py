174d878d4b65b4cc29de4b087bee64543289876d
johnyzaguirre <johnyzaguirre@johnyzaguirres-iMac.local>
2020-11-09 15:53:42 -0500

    updated guestbook tests

--- guestbook/tests/unit_test.py
+++ guestbook/tests/unit_test.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from guestbook import views
+from guestbook import models
 
 # http request -> url
 # url rules decide which view function resolve the url
@@ -39,13 +40,34 @@ class GuestbookViewTest(TestCase):
 
     def test_url_resolves_to_correct_view(self):
         view = resolve('/guestbook/view/')
-        assert view.func.__name__ == views.GuestCreateView.as_view().__name__
+        assert view.func.__name__ == views.GuestListView.as_view().__name__
 
     def test_uses_correct_template(self):
         response = self.client.get('/guestbook/view/')
-        self.assertTemplateUsed(response, 'guestbook/view_guest.html')
+        self.assertTemplateUsed(response, 'guestbook/guestbook.html')
 
     def test_has_correct_header(self):
         response = self.client.get('/guestbook/view/')
         self.assertContains(
-            response, "<h2 id='header'>Sign our Guestbook</h2>")
+            response, "<h2 id='header'>Guestbook</h2>")
+
+
+class GuestModelTest(TestCase):
+
+    def test_saving_and_retrieving_guests(self):
+        guest_one = models.Guest()
+        guest_one.name = 'John Smith'
+        guest_one.save()
+
+        guest_two = models.Guest()
+        guest_two.name = 'Jane Doe'
+        guest_two.save()
+
+        saved_items = models.Guest.objects.all()
+        assert saved_items.count() == 2
+
+        saved_guest_one = saved_items[0]
+        saved_guest_two = saved_items[1]
+
+        assert saved_guest_one.name == 'John Smith'
+        assert saved_guest_two.name == 'Jane Doe'
