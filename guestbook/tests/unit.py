edc0a78fa987271c19a95334714f24c343024aa8
johnyzaguirre <johnyzaguirre@johnyzaguirres-iMac.local>
2020-11-09 13:37:44 -0500

    added a few more tests. Updated readme

--- guestbook/tests/unit.py
+++ guestbook/tests/unit.py
@@ -11,7 +11,7 @@ from guestbook import views
 # can we resolve the url to a view?
 # can we make the view return html?
 
-class GuestbookAppTest(TestCase):
+class GuestbookNewTest(TestCase):
 
     def test_page_response_ok(self):
         assert self.client.get('/guestbook/new/').status_code == 200
@@ -26,4 +26,26 @@ class GuestbookAppTest(TestCase):
 
     def test_has_correct_header(self):
         response = self.client.get('/guestbook/new/')
-        assert "<h2 id='header'>Sign our Guestbook</h2>" in response.content.decode()
+        self.assertContains(response, "<h2 id='header'>Sign our Guestbook</h2>")
+
+    def test_form_submit_redirects_correctly(self):
+        response = self.client.post('/guestbook/new/', {'name': 'John Smith'}, follow=True)
+        self.assertContains(response, "<h2 id='header'>Guestbook</h2>")
+
+
+class GuestbookViewTest(TestCase):
+    def test_page_response_ok(self):
+        assert self.client.get('/guestbook/view/').status_code == 200
+
+    def test_url_resolves_to_correct_view(self):
+        view = resolve('/guestbook/view/')
+        assert view.func.__name__ == views.GuestCreateView.as_view().__name__
+
+    def test_uses_correct_template(self):
+        response = self.client.get('/guestbook/view/')
+        self.assertTemplateUsed(response, 'guestbook/view_guest.html')
+
+    def test_has_correct_header(self):
+        response = self.client.get('/guestbook/view/')
+        self.assertContains(
+            response, "<h2 id='header'>Sign our Guestbook</h2>")
--- readme.md
+++ readme.md
@@ -10,4 +10,19 @@ a page containing "mini apps"
 ### string reverse
 returns the user's submitted string in reverse
 
-**todo**-create more simple apps
+### guess
+guess a number from 1-10 and see if you match with the randomly generated number
+
+### guestbook
+sign into the guestbook and join the list of other guests who are probably just you
+
+# Installation
+- clone the repo
+- cd into the repo
+- python3 -m venv env
+- source env/bin/activate
+- pip install --upgrade pip
+- pip install -r requirements.txt
+- pytest to see happy green lines
+- python manage.py runserver
+- PROFIT!
